4093994ffff444c145d7a7fe5917e409fc2204b9
Hieu Ngo <hieu.ngo12989@gmail.com>
2022-09-08 13:16:06 -0700

    Completed Stack and Queue Functional Patterns

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -2,17 +2,21 @@ var Queue = function() {
   var someInstance = {};
 
   // Use an object with numeric keys to store values
-  var storage = {};
+  var storage = {
+    items: []
+  };
 
   // Implement the methods below
-
   someInstance.enqueue = function(value) {
+    storage.items.push(value);
   };
 
   someInstance.dequeue = function() {
+    return storage.items.shift();
   };
 
   someInstance.size = function() {
+    return storage.items.length;
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -2,17 +2,40 @@ var Stack = function() {
   var someInstance = {};
 
   // Use an object with numeric keys to store values
-  var storage = {};
+  var storage = {
+    items: [],
+    count: 0
+  };
 
   // Implement the methods below
   someInstance.push = function(value) {
+    storage.items[storage.count] = value;
+    storage.count ++;
   };
 
   someInstance.pop = function() {
+    if (storage.count <= 0) {
+      return 0;
+    }
+    var deleteItem = storage.items[storage.count - 1]
+    storage.count --;
+    return deleteItem;
   };
 
   someInstance.size = function() {
+    return storage.count;
   };
 
   return someInstance;
 };
+
+// var firstStack = Stack();
+// firstStack.push('2')
+// firstStack.push('3')
+// firstStack.pop()
+
+// firstStack.pop()
+// firstStack.push('1')
+
+// // firstStack.storage;
+// console.log(firstStack.storage)
