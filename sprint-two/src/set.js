f80ebd0fdd9f5e23aa41568a9ed2e87d91d790ef
Hieu Ngo <hieu.ngo12989@gmail.com>
2022-09-09 17:49:47 -0700

    Completed set.js

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,25 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = {};
   return set;
 };
 
 var setPrototype = {};
 
-setPrototype.add = function(item) {
+setPrototype.add = function(item) { // O(1)
+  this._storage[item] = item;
 };
 
-setPrototype.contains = function(item) {
+setPrototype.contains = function(item) { // O(1)
+  if (Object.keys(this._storage).includes(item)) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
-setPrototype.remove = function(item) {
+setPrototype.remove = function(item) { // 0(1)
+  delete this._storage[item]
 };
 
 /*
